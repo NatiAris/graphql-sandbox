fd88add5ad376d7c5dc330641240258954b90ed0
NatiAris <NatiAris@users.noreply.github.com>
2023-03-25 04:24:44 +0100

    idk smth

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema');
 const mongoose = require('mongoose');
 const app = express();
+const cors = require('cors');
+const path = require('path');
+
+app.use(cors());
 
 mongoose.connect('mongodb://root:example@mongodb:27017/petlibrary', {
   useNewUrlParser: true,
